0644dc438a69d48d1481360c6661274ae163ac50
wjdeogus03 <chung.daeh@northeastern.edu>
2023-11-14 17:31:30 -0800

    df

--- Database/index.js
+++ Database/index.js
@@ -1,12 +1,12 @@
-import courses from "./courses.json" assert { type: "json" };
-import modules from "./modules.json" assert { type: "json" };
-import assignments from "./assignments.json" assert { type: "json" };
-import users from "./users.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };
-import grades from "./grades.json" assert { type: "json" };
-import quizzes from "./quizzes.json" assert { type: "json" };
-import exams from "./exams.json" assert { type: "json" };
-import projects from "./projects.json" assert { type: "json" };
+const courses = require("./courses.json");
+const modules = require("./modules.json");
+const assignments = require("./assignments.json");
+const users = require("./users.json");
+const enrollments = require("./enrollments.json");
+const grades = require("./grades.json");
+const quizzes = require("./quizzes.json");
+const exams = require("./exams.json");
+const projects = require("./projects.json");
 
 
 export default {
