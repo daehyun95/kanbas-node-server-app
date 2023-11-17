c8d78a22bcc201905dcceb24f92359330df0f00f
wjdeogus03 <chung.daeh@northeastern.edu>
2023-11-17 13:41:51 -0800

    change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import AssignmentRoutes from "./assignments/routes.js";
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: "https://kanbas-node-server-app-cwch.onrender.com/"
+    origin: "*"
 
 })); // allow react to connect to server. and only allow 3000 to be called. 
 app.use(express.json()); // this is json parsing so the body knows what is passing in.
